f81d92599680da434ff078ed7e2c5be01b25f2b4
Ekaterina Mozhanova <pandafomka@gmail.com>
2024-10-29 01:41:52 +0300

    add icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { Link } from "@nextui-org/link";
+import { Camera } from 'lucide-react';
+
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
      <Button>Button</Button>
+     <Input/>
+      <Link color="secondary" isExternal showAnchorIcon>lick</Link>
+      <Camera color="red"/>
     </section>  
   );
 }
