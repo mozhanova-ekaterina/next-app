6a7ed7c09482dfedb5f16e3b5c04339e7662a6f6
Ekaterina Mozhanova <pandafomka@gmail.com>
2024-11-03 14:45:53 +0300

    saves

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   const body = await req.json()
+  
   const user = await prisma.user.create({ data: body })
   return NextResponse.json(user)
 }
